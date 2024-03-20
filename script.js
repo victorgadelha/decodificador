b652d09b55a74fdb9846a6f47d1fe819aec20b8d
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-03-19 21:07:01 -0300

    feat: Removendo caracteres especiais ao utilizar a função encrypt

--- script.js
+++ script.js
@@ -5,11 +5,24 @@ const decodedBox = document.querySelector(".decoded-box");
 const decodedContainer = document.querySelector(".decoded-container");
 
 const encrypt = () => {
-  const initialText = textBox.value;
+  const initialText = textBox.value
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "") // Remove acentos
+    .replace(/[^\w\s]/g, "") // Remove caracteres especiais, exceto espaços
+    .replace(/\s+/g, " "); // Remove espaços extras
 
-  if (!initialText) return;
-  decodedBox.classList.toggle("disabled");
-  decodedContainer.innerText = initialText;
+  if (initialText === "") {
+    decodedBox.classList.remove("disabled");
+    decodedContainer.classList.add("disabled");
+  } else {
+    decodedBox.classList.add("disabled");
+    decodedContainer.classList.remove("disabled");
+
+    decodedContainer.innerHTML = initialText;
+  }
+
+  console.log(initialText);
 };
 
 encryptButton.addEventListener("click", encrypt);
